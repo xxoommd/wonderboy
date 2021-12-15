56cd536d9a542203c3eb47e7282a39c54d092e50
gaohengyi <xxoommd@163.com>
2021-12-15 16:02:44 +0800

    clean code

--- main.go
+++ main.go
@@ -9,15 +9,15 @@ import (
 func main() {
 	var wg sync.WaitGroup
 	start := time.Now().UnixNano()
+
 	for i := 0; i < 2048; i++ {
 		wg.Add(1)
-		// index := i
 		go func() {
 			fib(33)
-			// fmt.Printf("[Goroutine:%d] fib(32)=%d \n", index, fib(32))
 			wg.Done()
 		}()
 	}
+
 	wg.Wait()
 
 	d := time.Now().UnixNano() - start
