65f44db2510bbcd5a40756ed396f72a86458b4be
gaohengyi <xxoommd@163.com>
2021-12-16 11:55:30 +0800

    Update docker running script

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -v "$PWD":/go/src/wonderboy -w /go/src/wonderboy golang:1.17-stretch go run main.go
+docker run --rm -v "$PWD":/go/src/wonderboy -w /go/src/wonderboy golang:1.17-stretch go run main.go
